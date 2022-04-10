1f820754d5d189e4e9668464dab8a843b2f52f16
Paulami <banerjee.paulami4@gmail.com>
2022-04-10 23:18:05 +0200

    Corrections and bug fixes

--- PSMNet-CSPN/predict.py
+++ PSMNet-CSPN/predict.py
@@ -6,27 +6,29 @@ import cv2
 
 
 def main():
-    left_img = ''
-    right_img = ''
+    left_img = '../input/sceneflow-driving1/DrivingDataTest/cleanpass/left/0051.png'
+    right_img = '../input/sceneflow-driving1/DrivingDataTest/cleanpass/right/0051.png'
 
     bat_size = 1
     maxdisp = 128
 
-    with tf.Session() as sess:
-        PSMNet = Model(sess, height=368, weight=1224, batch_size=bat_size, max_disp=maxdisp, lr=0.001)
-        saver = tf.train.Saver()
-        saver.restore(sess, './weights/PSMNet-CSPN.ckpt-600')
+    with tf.compat.v1.Session() as sess:
+        PSMNet = Model(sess, height=368, weight=1224, batch_size=bat_size, max_disp=maxdisp, lr=0.0001)
+        new_saver = tf.compat.v1.train.import_meta_graph('../input/retrain/results/PSMNet.ckpt-300.meta')
+        new_saver.restore(sess, tf.compat.v1.train.latest_checkpoint('../input/retrain/results/'))
+
 
         img_L = cv2.cvtColor(cv2.imread(left_img), cv2.COLOR_BGR2RGB)
-        img_L = cv2.resize(img_L, (368, 1224))
+        img_L = cv2.resize(img_L, (1224, 368))
         img_R = cv2.cvtColor(cv2.imread(right_img), cv2.COLOR_BGR2RGB)
-        img_R = cv2.resize(img_R, (368, 1224))
+        img_R = cv2.resize(img_R, (1224, 368))
 
         img_L = DataLoaderSF.mean_std(img_L)
         img_L = np.expand_dims(img_L, axis=0)
         img_R = DataLoaderSF.mean_std(img_R)
         img_R = np.expand_dims(img_R, axis=0)
         pred = PSMNet.predict(img_L, img_R)
+        pred = np.squeeze(pred)
 
         item = (pred * 255 / pred.max()).astype(np.uint8)
         pred_rainbow = cv2.applyColorMap(item, cv2.COLORMAP_RAINBOW)
@@ -34,4 +36,5 @@ def main():
 
 
 if __name__ == '__main__':
+    tf.compat.v1.reset_default_graph()
     main()
--- PSMNet-CSPN/train.py
+++ PSMNet-CSPN/train.py
@@ -8,9 +8,9 @@ import re
 import sys
 
 def main():
-    left_img = 'Data/Sample/left/'
-    right_img = 'Data/Sample/right/'
-    disp_img = 'Data/Sample/disparity/'
+    left_img = '../input/sceneflowdrivingtest/DrivingData/cleanpass/left/'
+    right_img = '../input/sceneflowdrivingtest/DrivingData/cleanpass/right/'
+    disp_img = '../input/sceneflowdrivingtest/DrivingData/disparity/'
 
     bat_size = 8
     # 128, 192
@@ -18,12 +18,9 @@ def main():
     epochs = 300
     dg = DataLoaderSF(left_img, right_img, disp_img, bat_size)
 
-    if not os.path.exists('./results/'):
-        os.mkdir('./results/')
-    with tf.Session() as sess:
-        PSMNet = Model(sess, height=256, weight=512, batch_size=bat_size,
-                       max_disp=maxdisp, lr=0.001)
-        saver = tf.train.Saver()
+    with tf.compat.v1.Session() as sess:
+        PSMNet = Model(sess, height=256, weight=512, batch_size=bat_size,max_disp=maxdisp, lr=0.001, cnn_3d_type='resnet_3d')
+        saver = tf.compat.v1.train.Saver()
         for epoch in range(1, epochs + 1):
             total_train_loss = 0
 
@@ -35,7 +32,7 @@ def main():
             avg_loss = total_train_loss / (160 // bat_size)
             print('epoch %d avg training loss = %.3f' % (epoch, avg_loss))
             if epoch % 30 == 0:
-                saver.save(sess, './results/PSMNet-CSPN.ckpt', global_step=epoch)
+                saver.save(sess, './results/PSMNet.ckpt', global_step=epoch)
 
             total_train_loss = 0
             for batch_idx, (imgL_crop, imgR_crop, disp_crop_L) in enumerate(dg.generator(is_training=False)):
@@ -46,8 +43,9 @@ def main():
 
             avg_loss = total_train_loss / (40 // bat_size)
             print('epoch %d avg testing loss = %.3f' % (epoch, avg_loss))
-        saver.save(sess, './results/PSMNet-CSPN.ckpt')
+        saver.save(sess, './results/PSMNet.ckpt')
 
 
 if __name__ == '__main__':
+    tf.compat.v1.reset_default.graph()
     main()
